a76079aedb8bf3065b10d78521dc65307a6844e9
ealpha072 <ealpha072@gmail.com>
2021-11-07 21:00:17 +0300

    Solved token error

--- new-game/main.js
+++ new-game/main.js
@@ -1,5 +1,37 @@
+//btns
+
+let rockbtn = $('#rock')
+let paperbtn = $('#paper')
+let scissorbtn = $('#scissors')
+let btns = [$('button')]
+
+
 function getComp() {
-  // body...
-  let myarr = ['rock','paper','scissors']
-  return Math.floor(Math.random()*myarr.length)
+  let myarr = ['rock',
+    'paper',
+    'scissors']
+  return myarr[Math.floor(Math.random()*myarr.length)]
 }
+
+function play(user, comp) {
+  comp = getComp()
+  user = 'rock'
+  let roundresults;
+  if (user == comp) {
+    roundresults = 'Draw'
+  }else {
+    if ( (user == 'rock' && comp == 'paper') || (user == 'paper' && comp == 'scissors') || (user == 'scissors' && comp == 'rock')){
+      roundresults = 'Comp wins'
+    }else {
+      roundresults = 'User wins'
+    }
+  }
+  return `User:${user}. Comp:${comp}. results:${roundresults}`
+}
+
+/*btns.forEach(btn => {
+  btn.on('click',function(){
+    var value = $(this).val()
+    console.log(value)
+  })
+})*/
