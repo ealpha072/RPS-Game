3204e3f1f32be1acc6f311c55c8a11042e1a957d
ealpha072 <ealpha072@gmail.com>
2021-11-09 11:25:10 +0300

    Refactored code with short hand declarations

--- new-game/main.js
+++ new-game/main.js
@@ -1,42 +1,32 @@
 
-let rockbtn = $('#rock')
-let paperbtn = $('#paper')
-let scissorbtn = $('#scissors')
-let btns = [$('.btn-block')]
+let rockbtn = $('#rock'), paperbtn = $('#paper'), scissorbtn = $('#scissors'), btns = [$('.btn-block')]
 
 //round variables
-let roundwinner = $('#roundwinner')
-let compchoice = $('#compchoice')
-let userchoice = $('#userchoice')
+let roundwinner = $('#roundwinner'), compchoice = $('#compchoice'), userchoice = $('#userchoice')
 
 //game variables
-let scoreuser = $('#userscore')
-let scorecomp =  $('#compscore')
-let gamesplayed = $('#gamesplayed')
-let gamewinner =  $('#winner')
+let scoreuser = $('#userscore'), scorecomp =  $('#compscore'), gamesplayed = $('#gamesplayed'), gamewinner =  $('#winner')
 
-let userscore = 0
-let compscore = 0
-let gamecount = 0
+let userscore = 0, compscore = 0, gamecount = 0
 
 
 function getComp() {
-	let myarr = ['rock','paper','scissors','rock','paper','scissors']
+	let myarr = ['rock','paper','scissors']
 	return myarr[Math.floor(Math.random()*myarr.length)]
 }
 
 function singleRound(user, comp) {
-  let roundresults;
-  if (user == comp) {
-    roundresults = 0
-  }else {
-    if ( (user == 'rock' && comp == 'paper') || (user == 'paper' && comp == 'scissors') || (user == 'scissors' && comp == 'rock')){
-      roundresults = 1 //comp wins
-    }else {
-      roundresults = 2 //user wins
-    }
-  }
-  return roundresults
+	let roundresults;
+	if (user == comp) {
+    	roundresults = 0
+  	}else {
+    	if ( (user == 'rock' && comp == 'paper') || (user == 'paper' && comp == 'scissors') || (user == 'scissors' && comp == 'rock')){
+      		roundresults = 1 //comp wins
+    	}else {
+      		roundresults = 2 //user wins
+    	}
+  	}
+	return roundresults
 }
 
 function endGame(){
@@ -55,9 +45,7 @@ function resetGame(){
 	gamesplayed.html('')
 	gamewinner.html('')
 
-	userscore = 0
-	compscore = 0
-	gamecount = 0
+	userscore = compscore = gamecount = 0
 
 	btns.forEach(btn=>{
 		btn.removeAttr('disabled')
@@ -76,9 +64,7 @@ $(function () {
 
 	btns.forEach((btn) => {
 		btn.on("click", function () {
-			let user = $(this).val();
-			let comp = getComp();
-			let winner = singleRound(user, comp);
+			let user = $(this).val(), comp = getComp(), winner = singleRound(user, comp);
 			if (winner == 0) {
 				gamecount++
 				roundwinner.html("");
